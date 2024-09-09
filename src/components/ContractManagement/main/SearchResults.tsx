8a302c8f23e4a2b277a4d23e2753beab38d58903
soyoon26 <barcardi26@gmail.com>
2024-09-09 22:37:13 +0900

    feat: add SearchResults Warning

--- src/components/ContractManagement/main/SearchResults.tsx
+++ src/components/ContractManagement/main/SearchResults.tsx
@@ -3,14 +3,13 @@ import {
   useTable,
   useRowSelect,
   HeaderGroup,
-  //   Row,
   Column as TableColumn,
   CellProps,
   HeaderProps,
-  //   ColumnInstance,
 } from "react-table";
 import SearchResultsBtn from "./SearchResultsBtn";
 import SearchResultsDropDown from "./SearchResultsDropDown";
+import WarningIcon from "./WarningIcon";
 
 type Data = {
   계약일: string;
@@ -30,34 +29,34 @@ type Data = {
 const SearchResults: React.FC = () => {
   const data: Data[] = React.useMemo(
     () => [
-      {
-        계약일: "24.08.29",
-        잔금일: "-",
-        만기일: "-",
-        계약서유형: "다세대",
-        거래유형: "매매",
-        소재지: "서울 서초구 양재동 8-26",
-        매매보증금: "-",
-        매도임대인: "-",
-        매수입차인: "-",
-        공동중개업소: "-",
-        계약관리: "-",
-        계약서번호: "A000111222",
-      },
-      {
-        계약일: "24.08.30",
-        잔금일: "-",
-        만기일: "-",
-        계약서유형: "연립",
-        거래유형: "전세",
-        소재지: "부산시 ",
-        매매보증금: "1,000원",
-        매도임대인: "김소금",
-        매수입차인: "박소윤",
-        공동중개업소: "스나이퍼부동산",
-        계약관리: "버튼 들어갈 자리",
-        계약서번호: "B000333444",
-      },
+      // {
+      //   계약일: "24.08.29",
+      //   잔금일: "-",
+      //   만기일: "-",
+      //   계약서유형: "다세대",
+      //   거래유형: "매매",
+      //   소재지: "서울 서초구 양재동 8-26",
+      //   매매보증금: "-",
+      //   매도임대인: "-",
+      //   매수입차인: "-",
+      //   공동중개업소: "-",
+      //   계약관리: "-",
+      //   계약서번호: "A000111222",
+      // },
+      // {
+      //   계약일: "24.08.30",
+      //   잔금일: "-",
+      //   만기일: "-",
+      //   계약서유형: "연립",
+      //   거래유형: "전세",
+      //   소재지: "부산시 ",
+      //   매매보증금: "1,000원",
+      //   매도임대인: "김소금",
+      //   매수입차인: "박소윤",
+      //   공동중개업소: "스나이퍼부동산",
+      //   계약관리: "버튼 들어갈 자리",
+      //   계약서번호: "B000333444",
+      // },
     ],
     []
   );
@@ -93,14 +92,10 @@ const SearchResults: React.FC = () => {
     []
   );
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-    // selectedFlatRows,
-  } = useTable<Data>({ columns, data }, useRowSelect);
+  const { getTableProps, headerGroups, rows, prepareRow } = useTable<Data>(
+    { columns, data },
+    useRowSelect
+  );
 
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
@@ -116,120 +111,140 @@ const SearchResults: React.FC = () => {
     setPageSize(size);
     setCurrentPage(1);
   };
+
   return (
-    <div className="w-[1067px] mx-auto">
-      <div className="flex items-center justify-between my-4">
-        <div className="font-bold">
-          검색결과 <span className="text-[#335995]">{data.length}</span>
+    <div className="w-[1142px] bg-white">
+      <div className="w-[1067px] pt-4  mx-auto">
+        <div className="flex items-center justify-between my-4">
+          <div className="font-bold">
+            검색결과 <span className="text-[#335995]">{data.length}</span>
+          </div>
+          <div className="flex gap-2">
+            {data.length >= 1 ? (
+              <>
+                <SearchResultsBtn
+                  text="개인정보 수집 및 이용 동의서 출력"
+                  borderColor="#335995"
+                  textColor="#335995"
+                  width={191}
+                  onClick={() => console.log("")}
+                />
+                <SearchResultsBtn
+                  text="엑셀다운로드"
+                  borderColor="#D9D9D9"
+                  textColor="black"
+                  width={96}
+                  onClick={() => console.log("")}
+                />
+                <SearchResultsDropDown
+                  selectedItem={`${pageSize}개씩 보기`}
+                  onSelect={handlePageSizeChange}
+                />
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
-        <div className="flex gap-2">
-          <SearchResultsBtn
-            text="개인정보 수집 및 이용 동의서 출력"
-            borderColor="#335995"
-            textColor="#335995"
-            width={191}
-            onClick={() => console.log("")}
-          />
-          <SearchResultsBtn
-            text="엑셀다운로드"
-            borderColor="#D9D9D9"
-            textColor="black"
-            width={96}
-            onClick={() => console.log("")}
-          />
-
-          <SearchResultsDropDown
-            selectedItem={`${pageSize}개씩 보기`}
-            onSelect={handlePageSizeChange}
-          />
-        </div>
-      </div>
-
-      <table
-        {...getTableProps()}
-        className="w-full "
-        style={{ maxWidth: "1067px" }}
-      >
-        <thead>
-          {headerGroups.map((headerGroup: HeaderGroup<Data>) => (
-            <tr {...(headerGroup.getHeaderGroupProps() as any)}>
-              {headerGroup.headers.map((column, columnIndex) => (
-                <th
-                  {...(column.getHeaderProps() as any)}
-                  className="font-bold text-center "
-                  style={{
-                    padding: "10px 5px",
-                    fontSize: "15px",
-                    backgroundColor: "#D9D9D9",
-                    borderTopLeftRadius: columnIndex === 0 ? "12px" : undefined,
-                    borderBottomLeftRadius:
-                      columnIndex === 0 ? "12px" : undefined,
-                    borderTopRightRadius:
-                      columnIndex === headerGroup.headers.length - 1
-                        ? "12px"
-                        : undefined,
-                    borderBottomRightRadius:
-                      columnIndex === headerGroup.headers.length - 1
-                        ? "12px"
-                        : undefined,
-                  }}
-                >
-                  {column.render("Header")}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...(getTableBodyProps() as any)} className="bg-white ">
-          {rows
-            .slice((currentPage - 1) * pageSize, currentPage * pageSize)
-            .map((row) => {
-              prepareRow(row);
-              return (
-                <tr {...(row.getRowProps() as any)}>
-                  {row.cells.map((cell) => (
-                    <td
-                      {...(cell.getCellProps() as any)}
-                      className="text-[12px] text-center font-bold  whitespace-nowrap"
-                      style={{ padding: "10px 5px" }}
+        <table
+          {...getTableProps()}
+          className="w-full mb-4"
+          style={{ maxWidth: "1067px" }}
+        >
+          <thead>
+            {headerGroups.map((headerGroup: HeaderGroup<Data>) => (
+              <tr
+                {...(headerGroup.getHeaderGroupProps() as React.HTMLProps<HTMLTableRowElement>)}
+              >
+                {headerGroup.headers.map((column, columnIndex) => (
+                  <th
+                    {...(column.getHeaderProps() as React.HTMLProps<HTMLTableHeaderCellElement>)}
+                    className="font-bold text-center "
+                    style={{
+                      padding: "10px 5px",
+                      fontSize: "15px",
+                      backgroundColor: "#D9D9D9",
+                      borderTopLeftRadius:
+                        columnIndex === 0 ? "12px" : undefined,
+                      borderBottomLeftRadius:
+                        columnIndex === 0 ? "12px" : undefined,
+                      borderTopRightRadius:
+                        columnIndex === headerGroup.headers.length - 1
+                          ? "12px"
+                          : undefined,
+                      borderBottomRightRadius:
+                        columnIndex === headerGroup.headers.length - 1
+                          ? "12px"
+                          : undefined,
+                    }}
+                  >
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+        </table>
+        {/* 검색결과 0 */}
+        {data.length === 0 ? (
+          <WarningIcon />
+        ) : (
+          <table {...getTableProps()} className="w-full bg-white">
+            <tbody>
+              {rows
+                .slice((currentPage - 1) * pageSize, currentPage * pageSize)
+                .map((row) => {
+                  prepareRow(row);
+                  return (
+                    <tr
+                      {...(row.getRowProps() as React.HTMLProps<HTMLTableRowElement>)}
                     >
-                      {cell.render("Cell")}
-                    </td>
-                  ))}
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
-
-      <div className="flex items-center justify-center p-2 mt-4 space-x-2 ">
-        <button
-          onClick={handleFirstPage}
-          className="w-[20px] h-[20px] border border-gray-300 text-gray-500 text-[12px] flex items-center justify-center rounded"
-        >
-          {"<<"}
-        </button>
-        <button
-          onClick={handlePreviousPage}
-          className="w-[20px] h-[20px] border border-gray-300 text-gray-500 text-[12px] flex items-center justify-center rounded"
-        >
-          {"<"}
-        </button>
-        <span className="w-[20px] h-[20px] flex items-center justify-center border border-[#335995] text-white bg-[#335995] text-[12px] rounded">
-          {currentPage}
-        </span>
-        <button
-          onClick={handleNextPage}
-          className="w-[20px] h-[20px] border border-gray-300 text-gray-500 text-[12px] flex items-center justify-center rounded"
-        >
-          {">"}
-        </button>
-        <button
-          onClick={handleLastPage}
-          className="w-[20px] h-[20px] border border-gray-300 text-gray-500 text-[12px] flex items-center justify-center rounded"
-        >
-          {">>"}
-        </button>
+                      {row.cells.map((cell) => (
+                        <td
+                          {...(cell.getCellProps() as React.HTMLProps<HTMLTableCellElement>)}
+                          className="text-[12px] text-center font-bold whitespace-nowrap"
+                          style={{ padding: "10px 5px" }}
+                        >
+                          {cell.render("Cell")}
+                        </td>
+                      ))}
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        )}
+        {data.length > 0 && (
+          <div className="flex items-center justify-center p-2 mt-4 space-x-2 ">
+            <button
+              onClick={handleFirstPage}
+              className="w-[20px] h-[20px] border border-gray-300 text-gray-500 text-[12px] flex items-center justify-center rounded"
+            >
+              {"<<"}
+            </button>
+            <button
+              onClick={handlePreviousPage}
+              className="w-[20px] h-[20px] border border-gray-300 text-gray-500 text-[12px] flex items-center justify-center rounded"
+            >
+              {"<"}
+            </button>
+            <span className="w-[20px] h-[20px] flex items-center justify-center border border-[#335995] text-white bg-[#335995] text-[12px] rounded">
+              {currentPage}
+            </span>
+            <button
+              onClick={handleNextPage}
+              className="w-[20px] h-[20px] border border-gray-300 text-gray-500 text-[12px] flex items-center justify-center rounded"
+            >
+              {">"}
+            </button>
+            <button
+              onClick={handleLastPage}
+              className="w-[20px] h-[20px] border border-gray-300 text-gray-500 text-[12px] flex items-center justify-center rounded"
+            >
+              {">>"}
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
