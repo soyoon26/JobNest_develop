bfd53f9f34dbb07c0599d2a0b3d68025d862925e
soyoon26 <barcardi26@gmail.com>
2024-09-10 23:01:13 +0900

    feat: 더미데이터 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // 프록시 설정
+      "/contract-list": {
+        target: "http://35.193.35.53",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/contract-list/, "/contract-list"),
+      },
+    },
+  },
+});
