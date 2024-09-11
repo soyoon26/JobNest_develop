ddbfdc8fd1a09f23b696935f7fcf0c5f1f73bef8
soyoon26 <barcardi26@gmail.com>
2024-09-11 17:11:00 +0900

    feat: kakao api 추가

--- src/components/ContractManagement/Drafting/Contract/Contract.tsx
+++ src/components/ContractManagement/Drafting/Contract/Contract.tsx
@@ -1,12 +1,15 @@
 const Contract = () => {
   return (
-    <>
-      <span>부동산(아파트) 매매 계약서</span>
-      <span>
+    <div className="bg-white">
+      <div></div>
+      <div className="w-[1223px] text-center h-[66px] rounded border border-gray text-[45px] font-bold">
+        부동산(아파트) 매매 계약서
+      </div>
+      <span className="text-[14px] font-bold">
         매도인과 매수인 쌍방은 아래 표시 부동산에 관하여 다음과 같이 매매 계약을
         체결한다.
       </span>
-    </>
+    </div>
   );
 };
 export default Contract;
---
+++ src/components/ContractManagement/Drafting/Receipt/Recipt.tsx
@@ -0,0 +1,8 @@
+const Recipt = () => {
+  return (
+    <>
+      <div className="w-[1395px] bg-slate-800">ㅎㅎ</div>
+    </>
+  );
+};
+export default Recipt;
