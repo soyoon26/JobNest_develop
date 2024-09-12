64ad7cc41b8a8129e094adff284d5ab99a574b4d
soyoon26 <barcardi26@gmail.com>
2024-09-12 17:45:48 +0900

    feat: 계약서 공통 버튼 추가

--- src/components/ContractManagement/Drafting/Common/ContractBtn.tsx
+++ src/components/ContractManagement/Drafting/Common/ContractBtn.tsx
@@ -1,7 +1,28 @@
-const ContractBtn = () => {
+interface ContractBtnProps {
+  color?: string;
+  borderColor?: string;
+  text?: string;
+  textColor?: string;
+  onClick?: () => void;
+}
+const ContractBtn: React.FC<ContractBtnProps> = ({
+  color = "white",
+  borderColor = "#D9D9D9",
+  text = "출력",
+  textColor = "#335995",
+}) => {
   return (
     <>
-      <button></button>
+      <button
+        style={{
+          backgroundColor: color,
+          borderColor: borderColor,
+          color: textColor,
+        }}
+        className="w-[105px] h-[41px] border rounded-md"
+      >
+        {text}
+      </button>
     </>
   );
 };
