6b6a9a34cc9777d0198fa32800a65b3d1208c64e
soyoon26 <barcardi26@gmail.com>
2024-09-15 22:49:56 +0900

    feat: 스피너 추가

---
+++ src/components/ContractManagement/main/Spinner.tsx
@@ -0,0 +1,12 @@
+// Spinner.tsx
+import React from "react";
+
+const Spinner: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-8 h-8 border-t-2 border-b-2 border-gray-500 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Spinner;
