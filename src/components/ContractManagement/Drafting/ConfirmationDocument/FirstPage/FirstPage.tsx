6068145e4100fd483d65e84666d3b3815074c739
soyoon26 <barcardi26@gmail.com>
2024-09-16 14:50:57 +0900

    feat: 확인서 유의사항  추가

--- src/components/ContractManagement/Drafting/ConfirmationDocument/FirstPage/FirstPage.tsx
+++ src/components/ContractManagement/Drafting/ConfirmationDocument/FirstPage/FirstPage.tsx
@@ -1,3 +1,4 @@
+import Notes from "./Notes";
 import TenantConfirmation from "./TenantConfirmation";
 
 const FirstPage = () => {
@@ -6,6 +7,7 @@ const FirstPage = () => {
       <div className="w-[1223px] text-center h-[66px] rounded border border-gray text-[45px] font-bold">
         중개대상물 확인 · 설명서[I](주거용 건축물)
       </div>
+      <Notes />
       <TenantConfirmation />
     </>
   );
