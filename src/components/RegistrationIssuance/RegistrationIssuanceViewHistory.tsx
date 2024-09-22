a92f936d3da45b5353690b5f0082b1fc72208e38
Jiwoo Park <jiwoopark727@gmail.com>
2024-09-22 16:31:31 +0900

    design: 등기/대장 열람 내역 디자인 수정

--- src/components/RegistrationIssuance/RegistrationIssuanceViewHistory.tsx
+++ src/components/RegistrationIssuance/RegistrationIssuanceViewHistory.tsx
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const RegistrationViewHistory = () => {
@@ -15,10 +14,10 @@ const RegistrationViewHistory = () => {
   };
 
   return (
-    <>
+    <div className='flex flex-col items-center'>
       <div className='pl-[65px] pt-[21px] w-full flex justify-between'>
         <div className='pt-[29px]'>
-          <span className='text-[35px] mb-[46px] font-extrabold'>
+          <span className='text-[35px] mb-[46px] font-extrabold select-none'>
             등기/대장 열람내역
           </span>
         </div>
@@ -33,44 +32,46 @@ const RegistrationViewHistory = () => {
           </button>
         </span>
       </div>
-      <div className='pl-[94px] pt-[73px] relative'>
-        <span className='pr-[20px]'>
-          <select
-            className='text-[#6f6f6f] cursor-pointer w-[100px] h-[40px] border border-[#cccccc] text-[14x] pl-[7px] py-[1px]'
-            name='doc'
-            id='type'
-          >
-            <option value='등기+대장'>전체</option>
-            <option value='등기'>등기</option>
-            <option value='대장'>대장</option>
-          </select>
-        </span>
-        <span className='relative'>
-          <input
-            type='text'
-            placeholder='주소를 입력해주세요.'
-            value={inputAddress}
-            className='w-[1000px] h-[40px] border border-[#cccccc] pl-[15px] pr-[30px]'
-            onChange={(e) => {
-              handleInputAddress(e.target.value);
-            }}
-          />
-          {inputAddress && (
-            <button
-              onClick={clearInput}
-              className='absolute right-[15px] top-[50%] transform -translate-y-1/2 text-gray-500'
+      <div className='w-[80%] mt-[42px]'>
+        <div className='flex flex-wrap'>
+          <span className='pr-[20px] grow-0 shrink-0 basis-[10%]'>
+            <select
+              className='text-[#6f6f6f] cursor-pointer w-[100px] h-[40px] border border-[#cccccc] text-[14x] pl-[7px] py-[1px]'
+              name='doc'
+              id='type'
             >
-              ×
+              <option value='전체'>전체</option>
+              <option value='등기'>등기</option>
+              <option value='대장'>대장</option>
+            </select>
+          </span>
+          <span className='relative grow-0 shrink-0 basis-[85%]'>
+            <input
+              type='text'
+              placeholder='주소를 입력해주세요.'
+              value={inputAddress}
+              className='w-[100%] h-[40px] border border-[#cccccc] pl-[15px] pr-[30px]'
+              onChange={(e) => {
+                handleInputAddress(e.target.value);
+              }}
+            />
+            {inputAddress && (
+              <button
+                onClick={clearInput}
+                className='absolute right-[15px] top-[50%] transform -translate-y-1/2 text-gray-500'
+              >
+                ×
+              </button>
+            )}
+          </span>
+          <span className='pl-[12px] relative grow-0 shrink-0 basis-[5%]'>
+            <button className='text-[14px] w-[48px] h-[40px] bg-[#347fff] text-white rounded font-normal'>
+              검색
             </button>
-          )}
-        </span>
-        <span className='pl-[12px] relative top-[-2px]'>
-          <button className='text-[11px] w-[48px] h-[40px] bg-[#347fff] text-white rounded'>
-            검색
-          </button>
-        </span>
+          </span>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 export default RegistrationViewHistory;
