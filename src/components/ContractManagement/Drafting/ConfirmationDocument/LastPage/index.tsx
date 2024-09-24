24e48f172f8fd0b1673da8f6a56201284580fba7
soyoon26 <barcardi26@gmail.com>
2024-09-24 10:05:59 +0900

    feat: 중개보수 맟 실비 산출

--- src/components/ContractManagement/Drafting/ConfirmationDocument/LastPage/index.tsx
+++ src/components/ContractManagement/Drafting/ConfirmationDocument/LastPage/index.tsx
@@ -1,11 +1,13 @@
 import ContractSignature from "./PeopleInfo";
 import HowToWrite from "./HowToWrite";
 import Signiture from "./Signiture";
+import CalculationDetails from "./CalculationDetails";
 
 const LsatPage = () => {
   return (
     <>
       <span>3. 중개보수 등에 관한 사항</span>
+      <CalculationDetails />
       <Signiture />
       <ContractSignature />
       <HowToWrite />
