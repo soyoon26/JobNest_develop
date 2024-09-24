d1099c2f38e090cc34bdd92a0559d2c41ebce818
soyoon26 <barcardi26@gmail.com>
2024-09-24 09:54:17 +0900

    feat: 서명

---
+++ src/components/ContractManagement/Drafting/ConfirmationDocument/LastPage/Signiture.tsx
@@ -0,0 +1,21 @@
+import SearchDate from "../../../Search/SearchDate";
+import ContractDate from "../../Common/ContractDate";
+import Contract from "../../Contract/Contract";
+
+const Signiture = () => {
+  return (
+    <div className="w-[1197px] border-by">
+      <div className="m-2">
+        「공인중개사법」 제25조제3항 및 제30조제5항에 따라 거래당사자는
+        개업공인중개사로부터 위 중개대상물에 관한 확인·설명 및 손해배상책임의
+        보장에 관한 설명을 듣고, 같은 법 시행령 제21조제3항에 따른 본
+        확인·설명서와 같은 법시행령 제24조제2항에 따른 손해배상책임 보장
+        증명서류(사본 또는 전자문서)를 수령합니다.
+      </div>
+      <div className=" m-2 flex justify-end w-full">
+        <ContractDate />
+      </div>
+    </div>
+  );
+};
+export default Signiture;
--- src/components/ContractManagement/Drafting/ConfirmationDocument/LastPage/index.tsx
+++ src/components/ContractManagement/Drafting/ConfirmationDocument/LastPage/index.tsx
@@ -1,10 +1,12 @@
 import ContractSignature from "./PeopleInfo";
 import HowToWrite from "./HowToWrite";
+import Signiture from "./Signiture";
 
 const LsatPage = () => {
   return (
     <>
       <span>3. 중개보수 등에 관한 사항</span>
+      <Signiture />
       <ContractSignature />
       <HowToWrite />
     </>
