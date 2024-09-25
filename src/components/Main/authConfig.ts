13cf4cc3dbd9e466b46667c43e66600aceed2de4
SeeunJoe <whtpdms1556@naver.com>
2024-09-26 04:01:09 +0900

    유효성 검사,중요도 추가

---
+++ src/components/Main/authConfig.ts
@@ -0,0 +1,19 @@
+/* export const msalConfig = {
+    auth: {
+      clientId: "7ccb8184-3b76-4d8b-b3a2-366b6cdc23bc", // Azure에서 생성한 앱의 클라이언트 ID
+      authority: "https://login.microsoftonline.com/fc4f8208-ec15-4944-bd5a-23cb94136963", // Azure AD의 테넌트 ID
+      redirectUri: "http://localhost:3000",
+    },
+    cache: {
+      cacheLocation: "localStorage", // 토큰이 저장될 위치, localStorage나 sessionStorage
+      storeAuthStateInCookie: true, // 쿠키에 인증 상태를 저장할지 여부 (IE11 지원 시 필요)
+    }
+  };
+
+  
+  // 로그인에 사용할 권한(Scopes)
+  export const loginRequest = {
+    scopes: ["User.Read", "Tasks.Read", "Tasks.ReadWrite","Tasks.Read.Shared", "Tasks.ReadWrite.Shared"] 
+  };
+
+   */
