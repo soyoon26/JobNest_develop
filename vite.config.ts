2ea92e1fa0a698689b41299ec724f6af6203660e
Jiwoo Park <jiwoopark727@gmail.com>
2024-10-02 21:52:19 +0900

    hotfix: 포트번호 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
     // proxy: {
     //   '/crolls': {
     //     target: 'http://35.193.35.53',
